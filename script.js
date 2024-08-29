15cb3a3864356f3eaa55e62a2d567fe56bb93963
alztr2908@gmail.com <alztr2908@gmail.com>
2024-08-29 18:14:04 +0800

    create myBoard with nxn matrix using flexbox (not user-defined yet)

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 console.log("Hello world");
 
 const myBoard = document.querySelector(".myBoard");
+const myButton = document.querySelector(".myButton");
+let SQUARE_NUM = 5;
+let BOARD_SIZE = 550;
+// myButton.addEventListener("click", (e) => {});
 
-for (let i = 0; i < 10 * 10; i++) {
+// Create board size
+for (let i = 0; i < SQUARE_NUM; i++) {
   const myBoardItem = document.createElement("div");
-  myBoardItem.classList.add("myBoard-item");
+  myBoardItem.classList.add("myBoardItem");
+  // Create square
+  for (let j = 0; j < SQUARE_NUM; j++) {
+    const myBoardItemChild = document.createElement("div");
+    myBoardItemChild.classList.add("myBoardItemChild");
+    myBoardItemChild.style.height = `${BOARD_SIZE / SQUARE_NUM}px`;
+    myBoardItemChild.style.width = `${BOARD_SIZE / SQUARE_NUM}px`;
+    myBoardItem.appendChild(myBoardItemChild);
+  }
+
   myBoard.appendChild(myBoardItem);
 }

2da31e26ff1f84ac41acb13b1da908a8e3840acc
alztr2908@gmail.com <alztr2908@gmail.com>
2024-08-29 18:29:42 +0800

    finish create myBoard with nxn matrix using flexbox (user-defined)

--- script.js
+++ script.js
@@ -2,22 +2,49 @@ console.log("Hello world");
 
 const myBoard = document.querySelector(".myBoard");
 const myButton = document.querySelector(".myButton");
-let SQUARE_NUM = 5;
-let BOARD_SIZE = 550;
-// myButton.addEventListener("click", (e) => {});
+let SQUARE_NUM = 16;
+const BOARD_SIZE = 550; //width and height of myBoard element (css)
 
-// Create board size
-for (let i = 0; i < SQUARE_NUM; i++) {
-  const myBoardItem = document.createElement("div");
-  myBoardItem.classList.add("myBoardItem");
-  // Create square
-  for (let j = 0; j < SQUARE_NUM; j++) {
-    const myBoardItemChild = document.createElement("div");
-    myBoardItemChild.classList.add("myBoardItemChild");
-    myBoardItemChild.style.height = `${BOARD_SIZE / SQUARE_NUM}px`;
-    myBoardItemChild.style.width = `${BOARD_SIZE / SQUARE_NUM}px`;
-    myBoardItem.appendChild(myBoardItemChild);
+myButton.addEventListener("click", () => {
+  const newSquareNum = parseInt(prompt("New Board Size? (Limit: 100)"));
+
+  removeBoard();
+  if (!isNaN(newSquareNum)) {
+    if (newSquareNum > 100) {
+      SQUARE_NUM = 100;
+    } else {
+      SQUARE_NUM = newSquareNum;
+    }
+    createBoard(SQUARE_NUM);
   }
 
-  myBoard.appendChild(myBoardItem);
-}
+  return;
+});
+
+const createBoard = (square) => {
+  for (let i = 0; i < square; i++) {
+    const myBoardItem = document.createElement("div");
+    myBoardItem.classList.add("myBoardItem");
+    // Create square
+    for (let j = 0; j < square; j++) {
+      const myBoardItemChild = document.createElement("div");
+      myBoardItemChild.classList.add("myBoardItemChild");
+      myBoardItemChild.style.height = `${BOARD_SIZE / square}px`;
+      myBoardItemChild.style.width = `${BOARD_SIZE / square}px`;
+      myBoardItem.appendChild(myBoardItemChild);
+    }
+
+    myBoard.appendChild(myBoardItem);
+  }
+
+  return;
+};
+
+// Remove all elements before creating a new one
+const removeBoard = () => {
+  while (myBoard.lastElementChild) {
+    myBoard.removeChild(myBoard.lastElementChild);
+  }
+};
+
+createBoard(SQUARE_NUM);

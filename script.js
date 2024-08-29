96f7eb4e7123bb440769a28fd961fde4bc208ca6
alztr2908@gmail.com <alztr2908@gmail.com>
2024-08-29 17:29:56 +0800

    create myBoard with nxn matrix (not user-defined)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello world");
+
+const myBoard = document.querySelector(".myBoard");
+
+for (let i = 0; i < 10 * 10; i++) {
+  const myBoardItem = document.createElement("div");
+  myBoardItem.classList.add("myBoard-item");
+  myBoard.appendChild(myBoardItem);
+}
